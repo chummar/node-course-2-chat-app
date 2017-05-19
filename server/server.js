e138adea506ab3dbdbb4df6a5b78db366c151289
ChummarMaly <chummar@gmail.com>
2017-05-19 13:36:01 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+console.log(publicPath);
+
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+
+  console.log(`Server Started on port ${port}`);
+
+});
+
+module.exports = {app};
